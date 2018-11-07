4a8fc8b3fbc23674675edaf197db4eec45a579a2
BalayetRony <44699553+BalayetRony@users.noreply.github.com>
2018-11-07 12:16:51 +0100

    Add files via upload

---
+++ inc.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  7 12:00:29 2018
+
+@author: mdb99
+"""
+
+def inc(userinput):
+    return userinput +1
+
+print(inc(10))
---
+++ unttst.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  7 12:06:34 2018
+
+@author: mdb99
+"""
+import unittest 
+import inc 
+# import inc
+class MyTest(unittest.TestCase):
+    def test_my_function(self):
+        self.assertEqual(inc(0), 1)
+        self.assertEqual(inc(100), 101)
+        self.assertEqual(inc(1), 2)
+        self.assertEqual(inc(-1), 0)
+        self.assertEqual(inc(3), 5)
+        
+        
+if __name__ == '__main__':
+    unittest.main()   
